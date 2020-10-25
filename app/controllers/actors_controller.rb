5756e7215d2728902adb4289b3c8765c32d9811c
rhetherington26 <rhetheri@chicagobooth.edu>
2020-10-25 00:18:02 +0000

    Final Commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,6 +8,7 @@ class ActorsController < ApplicationController
   def bio
     @instance = params.fetch("actor_id")
     @actor = Actor.all.where( :id => @instance).at(0)
+    @characters = Character.all.where( :actor_id => @instance)
    render({ :template => "actors_templates/bio.html.erb"})
   end
   
